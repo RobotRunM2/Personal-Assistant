4f87e02428902ac86ab7b418076165429358b195
xiaocao <wdjoys@hotmail.com>
2022-04-07 14:38:44 +0800

    🔧 环境、依赖

---
+++ src/main.py
@@ -0,0 +1,23 @@
+"""
+Author: wdjoys
+Date: 2022-04-07 11:05:20
+LastEditors: wdjoys
+LastEditTime: 2022-04-07 11:20:05
+FilePath: \Personal-Assistant\src\main.py
+Description: 项目入口
+
+Copyright (c) 2022 by github/wdjoys, All Rights Reserved. 
+"""
+
+
+def run():
+    """
+    项目入口
+    """
+    from src.main import main
+
+    main()
+
+
+if __name__ == "__main__":
+    pass
